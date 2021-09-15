c924e8c5e8cc619613897d48cf6d62a5400da635
root <aminmohamedadan@gmail.com>
2021-09-15 09:07:22 +0000

    returns a list with values multiplied by a number

---
+++ 0x04-python-more_data_structures/11-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import multiply_list_map
+
+    my_list = [1, 2, 3, 4, 6]
+    new_list = multiply_list_map(my_list, 4)
+    print(new_list)
+    print(my_list)
